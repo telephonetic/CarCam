9080580da5313e665a5d25ecf548706663f77049
telephonetic <demonacles@gmail.com>
2017-12-02 18:57:38 -0600

    Update record.sh
    These changes will stop recording every 5 minutes and start the script over again. That way you get multiple smaller files than one large one, additionally if you are recording and something happens while driving you don't have to worry about your entire video being corrupted.

--- CamProj/scripts/shell/record.sh
+++ CamProj/scripts/shell/record.sh
@@ -13,9 +13,9 @@ do
 	echo "New FileName: " "$new_fileName"
 	
 	#This was the toughest part of the whole project to find the optimal settings for recording.
-	avconv -f video4linux2 -r 16 -s 640x480 -i /dev/video0 -c:v mpeg4 -r 16 -an -s 640x480 -b 1024k -t 00:30:30 -y $new_fileName
+	timeout 300 avconv -f video4linux2 -r 16 -s 640x480 -i /dev/video0 -c:v mpeg4 -r 16 -an -s 640x480 -b 1024k -t 00:30:30 -y $new_fileName
 
 	echo "Done with recording..."
 
 done
- 
+./$(basename $0) && exit
